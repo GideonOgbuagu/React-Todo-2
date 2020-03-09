bb9accf4eabf51c14db8c8642ac49102a7a0472c
Gideon <gideon@Mac-1s-MBP.home>
2020-03-09 17:56:01 -0500

    Created first skeletal structure for components

---
+++ src/components/Todo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todo = () => {
+    return (
+        <div>
+            <p>Hello from Todo.js</p>
+        </div>
+    )
+}
+
+export default Todo;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class TodoForm extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                 
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                Hello From TodoForm
+            </div>
+        )
+    }
+}
+
+export default TodoForm
